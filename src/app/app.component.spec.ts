9ba13580442dc89fedf8a58e03e593798356b41f
Santu Koley <santu.cst08@gmail.com>
2017-03-22 11:55:06 +0530

    innitial import

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,19 +2,23 @@
 
 import { addProviders, async, inject } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+//import { FormsModule } from '@angular/forms';
+import { Todo } from './todo';
+import { TodoDataService } from './todo-data.service';
+
 
 describe('App: TodoApp', () => {
   beforeEach(() => {
-    addProviders([AppComponent]);
+    addProviders([AppComponent, ]);
   });
 
-  it('should create the app',
-    inject([AppComponent], (app: AppComponent) => {
-      expect(app).toBeTruthy();
-    }));
+  // it('should create the app',
+  //   inject([AppComponent], (app: AppComponent) => {
+  //     expect(app).toBeTruthy();
+  //   }));
 
-  it('should have as title \'app works!\'',
-    inject([AppComponent], (app: AppComponent) => {
-      expect(app.title).toEqual('app works!');
-    }));
+  // it('should have a newTodo todo',
+  //   inject([AppComponent], (app: AppComponent) => {
+  //     expect(app.newTodo instanceof Todo).toBeTruthy();
+  //   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import {Todo} from './todo';
+import {TodoDataService} from './todo-data.service';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'app works!';
+
+  newTodo: Todo = new Todo();
+
+  constructor(private _todoDataService: TodoDataService){}
+
+  addTodo() {
+    this._todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo) {
+    this._todoDataService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    this._todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this._todoDataService.getAllTodos();
+  }
 }
